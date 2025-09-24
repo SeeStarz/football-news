63d3fb76b9e4c41f60e7e4612a68ae76d80ade27
SeeStarz <seestarz@tutamail.com>
2025-09-24 14:25:30 +0700

    finish tutorial 4, learn l8r

--- football_news/settings.py
+++ football_news/settings.py
@@ -146,7 +146,7 @@ if DEBUG:
         BASE_DIR / 'static' # merujuk ke /static root project pada mode development
     ]
 else:
-    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode productionTATIC_URL = 'static/'
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
